0df5967458b0b207893d060182598f48496ffeac
oggunderscore <oggunderscore@gmail.com>
2023-10-25 17:06:43 -0700

    Fixed index formatting

--- Lab07/main.py
+++ Lab07/main.py
@@ -14,31 +14,33 @@ while True:
     choice = input("Enter your choice: ")
 
     if choice == "1":
-        first_name = input("Enter faculty's first name: ")
-        last_name = input("Enter faculty's last name: ")
-        department = input("Enter faculty's department: ")
+        first_name = input("Enter first name: ")
+        last_name = input("Enter last name: ")
+        department = input("Enter department: ")
 
         faculty = Faculty(first_name, last_name, department)
         faculty_list.append(faculty)
-        print("Faculty added.")
+        print("\nFaculty added.")
 
     elif choice == "2":
         print("\n======================= FACULTY =======================")
         print(f"Record  {'Name':20}  {'Department':25}")
         print("======  ====================  =========================")
         for index, faculty in enumerate(faculty_list, start=1):
+            index = str(index)  # Cast to Str to format left
             print(
                 f"{index:6}  {(faculty.firstname + ' ' + faculty.lastname):20}  {faculty.department:25}"
             )
 
     elif choice == "3":
-        first_name = input("Enter student's first name: ")
-        last_name = input("Enter student's last name: ")
-        class_year = input("Enter student's class year: ")
-        major = input("Enter student's major: ")
+        first_name = input("Enter first name: ")
+        last_name = input("Enter last name: ")
+        class_year = input("Enter class year: ")
+        major = input("Enter major: ")
 
-        print("Choose a faculty advisor from the list:")
+        print("Choose a faculty advisor from the list:\n")
         for index, faculty in enumerate(faculty_list, start=1):
+            index = str(index)  # Cast to Str to format left
             print(
                 f"{index:6}  {(faculty.firstname + ' ' + faculty.lastname):20}  {faculty.department:25}"
             )
@@ -53,9 +55,9 @@ while True:
             student.set_major(major)
             student.set_advisor(faculty_list[advisor_index])
             student_list.append(student)
-            print("Student added.")
+            print("\nStudent added.")
         else:
-            print("Invalid faculty advisor selection. Student not added.")
+            print("\nInvalid faculty advisor selection. Student not added.")
 
     elif choice == "4":
         print(
